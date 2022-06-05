90908983b9d26c584b95ac02defdbf5cde1f3802
alameenabolare <55580153+alameenabolare@users.noreply.github.com>
2022-06-05 16:46:16 +0100

    Click function added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).keydown(function(event){
+$(".button").click(function(event){
   var rand = Math.floor(Math.random()*13) + 78;
   var audio = new Audio("sounds/" + rand + ".mp3");
   audio.play();
