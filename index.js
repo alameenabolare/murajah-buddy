b8c8cf89e3e1dc269f772544777f949ad995556a
alameenabolare <55580153+alameenabolare@users.noreply.github.com>
2022-06-07 22:19:10 +0100

    g

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-$(".button").click(function(event){
+var count = 0;
+
+$(".title-btn").click(function(event){
+  count++;
   var rand = Math.floor(Math.random()*13) + 78;
   var audio = new Audio("sounds/" + rand + ".mp3");
   audio.play();
+  console.log(count)
 })
+
+// if (count === 1) {
+//   // $(".hintBtn").style.visibility = "visible";
+//   // $(".hintBtn").click(function(){
+//     $(".hint").text("Hint: The verse is in Chapter " + rand + " of the Qur'an");
+//   // });
+// }
