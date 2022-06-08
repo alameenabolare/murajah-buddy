f1f87d26d2863d2a0674093c409db393adab687f
alameenabolare <55580153+alameenabolare@users.noreply.github.com>
2022-06-08 15:19:25 +0100

    Update

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-Quran.init();
-
 var count = 0;
 
 $(".title-btn").click(function(event){
   count++;
-  // var rand = Math.floor(Math.random()*995) + 5242;
-  // var audio = new Audio("https://cdn.islamic.network/quran/audio/128/ar.minshawi/" + rand + ".mp3");
-
   var rand = Math.floor(Math.random()*13) + 78;
   var audio = new Audio("sounds/" + rand + ".mp3");
   audio.play();
@@ -14,7 +9,6 @@ $(".title-btn").click(function(event){
   $(".hintBtn").removeClass("removeHintBtn");
 
   $(".hintBtn").click(function(){
-
     $(".hint").text("Hint: The verse is in Surat " + Quran.surah.name(rand, 'english_name') + " of the Qur'an");
     setTimeout(function(){
       $(".hint").text("");
